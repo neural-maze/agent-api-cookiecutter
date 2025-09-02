1f0c394d9101404d33d3cfd5389ab6a83b05d626
MiguelOtero <miguel.otero@dressipi.com>
2025-09-02 19:23:31 +0200

    fix: reformat code following ruff

--- {{cookiecutter.package_name}}/src/{{cookiecutter.project_slug}}/infrastructure/api/main.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.project_slug}}/infrastructure/api/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
-from .models import ChatRequest, EvalRequest, IngestDocumentsRequest, ResetMemoryRequest
 
 from fastapi import FastAPI
+
+from .models import ChatRequest, EvalRequest, IngestDocumentsRequest, ResetMemoryRequest
 
 
 @asynccontextmanager
@@ -9,21 +10,23 @@ async def lifespan(app: FastAPI):
     """Handles startup and shutdown events for the API."""
     # Startup code (if any) goes here
     yield
-    
+
 
 app = FastAPI(
-    title=" {{ cookiecutter.project_name }} ",
-    description=" {{ cookiecutter.project_short_description }} ",
+    title=" Agent API ",
+    description=" This is a template for creating powerful Agent APIs. ",
     docs_url="/docs",
     lifespan=lifespan,
 )
+
 
 @app.get("/")
 async def root():
     """
     Root endpoint that redirects to API documentation
     """
-    return {"message": "Welcome to {{ cookiecutter.project_name }}. Visit /docs for documentation"}
+    return {"message": "Welcome to Agent API. Visit /docs for documentation"}
+
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
@@ -42,14 +45,6 @@ async def eval(request: EvalRequest):
 
 
 @app.post("/eval")
-async def ingest_documents(request: IngestDocumentsRequest):
-    """
-    Uses the Chat Service from application layer
-    """
-    return {"message": "Ingest documents request received"}
-
-
-@app.post("/ingest-documents")
 async def ingest_documents(request: IngestDocumentsRequest):
     """
     Uses the Chat Service from application layer
--- {{cookiecutter.package_name}}/src/{{cookiecutter.project_slug}}/infrastructure/api/models.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.project_slug}}/infrastructure/api/models.py
@@ -4,11 +4,14 @@ from pydantic import BaseModel
 class ChatRequest(BaseModel):
     pass
 
+
 class EvalRequest(BaseModel):
     pass
 
+
 class IngestDocumentsRequest(BaseModel):
     pass
+
 
 class ResetMemoryRequest(BaseModel):
     pass
