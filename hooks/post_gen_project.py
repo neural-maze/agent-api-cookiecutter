150cf5140b31f5effa9068aac1823d3b7ca724e3
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-09-02 16:00:30 +0200

    feat: first commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    print("Your Agent API project has been created successfully!")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from agent-api-cookiecutter!")
+
+
+if __name__ == "__main__":
+    main()
