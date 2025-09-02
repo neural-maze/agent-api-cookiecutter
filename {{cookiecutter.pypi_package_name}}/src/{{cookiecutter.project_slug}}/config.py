e93fba0e5e1ac454922d7970e3d46c9bdc6dcc40
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2025-09-02 16:31:12 +0200

    feat: add more files to project structure

---
+++ {{cookiecutter.pypi_package_name}}/src/{{cookiecutter.project_slug}}/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", extra="ignore", env_file_encoding="utf-8"
+    )
+
+
+settings = Settings()
